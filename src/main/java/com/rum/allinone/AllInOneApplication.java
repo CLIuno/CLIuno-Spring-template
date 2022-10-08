e226f87b307c5a4b3bbd358de07f4fa72610937f
RuM <18311071+ru44@users.noreply.github.com>
2022-10-08 11:11:22 +0300

    init commit

---
+++ src/main/java/com/rum/allinone/AllInOneApplication.java
@@ -0,0 +1,13 @@
+package com.rum.allinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllInOneApplication.class, args);
+	}
+
+}
